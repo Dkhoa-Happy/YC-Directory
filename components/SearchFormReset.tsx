39e6c9c6731ac134fcbe2d79acaf591d0c3acf72
Dkhoa_Happy <dangkhoa13978@gmal.com>
2024-11-28 12:09:04 +0700

    Set up Sanity

--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -3,17 +3,18 @@
 import React from 'react'
 import Link from "next/link";
 import {X} from "lucide-react";
+
 const SearchFormReset = () => {
     const reset = () => {
         const form = document.querySelector('.search-form') as HTMLFieldSetElement;
 
-        if(form) form.reset();
+        if (form) form.reset();
     }
     return (
         <button type="reset" onClick={reset}>
-        <Link href="/" className="search-btn text-white">
-            <X className="size-5" />
-        </Link>
+            <Link href="/" className="search-btn text-white">
+                <X className="size-5"/>
+            </Link>
         </button>
     )
 }
