b85059e270e31bfecf21959ed8934a21744ea2c0
Dkhoa_Happy <dangkhoa13978@gmal.com>
2024-12-04 22:14:31 +0700

    Done Next JS Course

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,24 +1,36 @@
-import React from "react";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import { notFound } from "next/navigation";
+import { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
 import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
+
 import markdownit from "markdown-it";
-import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
 
 export const experimental_ppr = true;
+
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, editorPostsResponse] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks-new" }),
+  ]);
+
   if (!post) return notFound();
+
+  const editorPosts = editorPostsResponse?.select || [];
   const parsedContent = md.render(post?.pitch || "");
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
@@ -26,12 +38,14 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
+
       <section className="section_container">
         <img
           src={post.image}
           alt="thumbnail"
           className="w-full h-auto rounded-xl"
         />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
@@ -52,8 +66,10 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 </p>
               </div>
             </Link>
+
             <p className="category-tag">{post.category}</p>
           </div>
+
           <h3 className="text-30-bold">Pitch Details</h3>
           {parsedContent ? (
             <article
@@ -66,7 +82,21 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
         </div>
 
         <hr className="divider" />
-        {/*TODO: EDITOR SELECTED STARTUPS*/}
+
+        {editorPosts.length > 0 ? (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p className="no-result">No editor picks available</p>
+        )}
+
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense>
@@ -74,4 +104,5 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     </>
   );
 };
+
 export default Page;
